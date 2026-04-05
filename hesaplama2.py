e4b49553d63f4612c0ca70f009d6fa50c200a964
Kaan Efe Ay <kaanefe.rr520@gmail.com>
2026-04-05 23:41:00 +0300

    hesaplama2

---
+++ hesaplama2.py
@@ -0,0 +1,12 @@
+a = 3
+b = 4
+c = 5
+
+print("Robotik Dersi Hesaplama Odevi")
+print("----------------------------")
+print("A degeri:", a)
+print("B degeri:", b)
+print("C degeri:", c)
+print("----------------------------")
+print("A + B + C Toplami:", a + b + c)
+print("A - B Farki:", a - b)
